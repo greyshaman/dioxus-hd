b81f3872f0a03aa4209d21df34ee2ca29a8240fc
Sergey Reshetnikov <shaman@simmirra.com>
2025-11-05 14:32:45 +0300

    removed unused consts

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,7 @@ mod components;
 
 use crate::components::*;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
-const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
     dioxus::launch(App);
